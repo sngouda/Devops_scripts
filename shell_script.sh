3b4a827f8cc45fa0e643fc10639daad942354ebb
sngouda <suhasgowda636227@gmail.com.com>
2026-08-20 15:55:07 +0000

    Merge remote and local changes

---
+++ shell_script.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+
+# Simple Calculation operation
+
+echo "Perform the calculation Task"
+
+echo "what is the sum of a+b?"
+read a
+read b
+let sum=a+b
+echo "The sum of  $a and $b is $sum "
+
